465066509f01566dfef60e38cbde5c07b3706572
David Lane <david.lane@canonical.com>
2023-03-01 16:19:17 +1100

    Super simple dbus client server apps

---
+++ dbus_client.py
@@ -0,0 +1,21 @@
+#!/bin/env python3
+import sys
+
+if len(sys.argv) < 2:
+    name = "<no name>"
+else:
+    name = sys.argv[1]
+
+
+from dasbus.connection import SessionMessageBus
+bus = SessionMessageBus()
+
+
+proxy = bus.get_proxy(
+    "org.dclane.Dave",
+    "/org/dclane/Dave"
+)
+
+print("Response: {}".format(proxy.Hello(name)))
+
+
---
+++ dbus_server.py
@@ -0,0 +1,24 @@
+#!/bin/env python3
+
+from dasbus.server.interface import dbus_interface
+from dasbus.typing import Str
+
+@dbus_interface("org.dclane.Dave")
+class Dave(object):
+
+    def Hello(self, name: Str) -> Str:
+        print("msg recv'd")
+        return "Hello {}!".format(name)
+
+print(Dave.__dbus_xml__)
+
+
+from dasbus.connection import SessionMessageBus
+bus = SessionMessageBus()
+
+bus.publish_object("/org/dclane/Dave", Dave())
+bus.register_service("org.dclane.Dave")
+
+from dasbus.loop import EventLoop
+loop = EventLoop()
+loop.run()
